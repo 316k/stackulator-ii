c6928be49583f77fc2b97cfc88198b8fc02c2593
Nicolas Hurtubise <316k@legtux.org>
2015-10-01 12:23:48 -0400

    Corrects struct syntax

--- bignum.c
+++ bignum.c
@@ -1,13 +1,13 @@
 // Note : little-endian numbers
-struct bignum {
-    short sign,
-    bigchiffre chiffre
-};
-
 struct bigchiffre {
-    short valeur,
-    bigchiffre* next
+    char valeur;
+    struct bigchiffre* next;
 };
+typedef struct bigchiffre bigchiffre;
 
+struct bignum {
+    char sign;
+    struct bigchiffre chiffre;
+};
 typedef struct bignum bignum;
 
