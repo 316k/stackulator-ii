d1a7076d42e0bdf71bcda49eb18d8b0431d0d34c
Guillaume <Guillaume Riou>
2015-10-02 14:16:48 -0400

    Started the implementation of the stack.

--- stack.c
+++ stack.c
@@ -1,1 +1,19 @@
 // Requires bignum.c to be included first
+
+typedef struct {
+    char element;
+    char *next_element;
+} stack_cell;
+
+typedef struct {
+    stack_cell *top;
+} stack;
+
+void push(stack *the_stack ,char element){
+    stack_cell new_cell = {element, the_stack->top};
+    the_stack->top = &new_cell;
+}
+
+int main(){
+    return EXIT_SUCCESS;
+}
