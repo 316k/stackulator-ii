0bcbe2cb74cd8909ff3a8f5ef9f23fb76f55e85c
Guillaume <Guillaume Riou>
2015-10-02 14:25:11 -0400

    i:qwith '#' will be ignored, and an empty message aborts the commit.

--- stack.c
+++ stack.c
@@ -1,19 +1,20 @@
 // Requires bignum.c to be included first
 
-typedef struct {
-    char element;
-    char *next_element;
-} stack_cell;
+typedef struct stack_cell stack_cell;
 
-typedef struct {
+struct stack_cell {
+    char element;
+    stack_cell *next_element;
+};
+
+struct stack {
     stack_cell *top;
-} stack;
+};
+
+typedef struct stack stack;
 
 void push(stack *the_stack ,char element){
     stack_cell new_cell = {element, the_stack->top};
     the_stack->top = &new_cell;
 }
 
-int main(){
-    return EXIT_SUCCESS;
-}
