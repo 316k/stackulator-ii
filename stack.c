f16bf812dca4fdec20fe24e8f5fe30d823c329fb
Guillaume <Guillaume Riou>
2015-10-06 12:50:16 -0400

    Adapts the stack for "smallnum" a fictive type.

--- stack.c
+++ stack.c
@@ -1,10 +1,15 @@
 // Requires bignum.c to be included first
 
 typedef struct stack_cell stack_cell;
+typedef struct smallnum smallnum;
+
+struct smallnum {
+    char element;
+};
 
 struct stack_cell {
-    char element;
-    stack_cell *next_element;
+    smallnum element;
+    stack_cell *next_cell;
 };
 
 struct stack {
@@ -13,26 +18,26 @@ struct stack {
 
 typedef struct stack stack;
 
-void push(stack *stack ,char element){
+void push(stack *stack ,smallnum element){
     stack_cell *new_cell = malloc(sizeof(stack_cell));
     new_cell->element = element;
-    new_cell->next_element = stack->top;
+    new_cell->next_cell = stack->top;
     stack->top = new_cell;
 }
 
-char pop(stack *stack) {
+smallnum pop(stack *stack) {
     stack_cell *popped = stack->top;
-    stack->top = popped->next_element;
-    char value = popped->element;
+    stack->top = popped->next_cell;
+    smallnum value = popped->element;
     free(popped); //like a bird.
     return value;
 }
 
-char peek(stack *stack) {
+smallnum peek(stack *stack) {
     return stack->top->element;
 }
 
-char empty(stack *stack) {
+int empty(stack *stack) {
     return stack->top == NULL;
 }
 
--- stackulator.c
+++ stackulator.c
@@ -9,6 +9,6 @@
 
 int main(int argc, char* argv[]) {
     test_all();
-    
+    //test_stack();
     return EXIT_SUCCESS;
 }
